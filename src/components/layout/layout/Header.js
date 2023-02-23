c4e04a455f75e6f3bc2dadbb6f7db7507f6eda86
axeladib <nabil298adib@gmail.com>
2023-02-23 22:36:59 +0800

    BUGS : FIXED : className not match with the declaration in App.scss file

--- src/components/layout/layout/Header.js
+++ src/components/layout/layout/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import {FaPizzaSlice} from "react-icons/fa";
+
 // Declare compnent using arrow function
 const Header = () => {
     return (
 <header className="header" data-testid="header">
     <nav >
-        <div className="header__logo">
+        <div className="logo">
             <img src="/images/logo.png" alt="Todoist"/>
         </div>
-        <div className="header__settings">
+        <div className="settings">
             <ul>
                 <li data-testid="quick-add-task-action" className="settings__add">+</li>
                 <li data-testid="dark-mode-action" className="settings__darkmode"><FaPizzaSlice/></li>
